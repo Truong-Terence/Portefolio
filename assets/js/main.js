4a41383f6864cad844c1b53621bf019104741f5b
Térence renard--Truong <tvn.terence@gmail.com>
2021-02-24 19:19:08 +0100

    Python

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,10 @@
 /**
- * 2 buttons linked to altert
- * PHP Not learned yet so there is no form
- * Allow to open the alert again with : display = "none" and not data-dismiss = "alert"
- */
-var alertPop = document.getElementById("alert")
-
-function PopTheAlert() {alertPop.style.display = "block"}
-
-document.getElementById("talkButton").addEventListener("click", PopTheAlert)
-document.getElementById("talkButton2").addEventListener("click", PopTheAlert)
-
-document.getElementById("closePop").addEventListener("click", function() {
-    alertPop.style.display = "none"
-})
-
-/**
  * Show and hide python project
  * Animation with keyframes
  */
 var trinket = document.querySelector(".hiddenTrinket")
-var showTrinket = document.querySelector(".showTrinket")
+var showTrinketCasino = document.querySelector(".showTrinketCasino")
+var showTrinketDico = document.querySelector(".showTrinketDico")
+var showTrinketHangman = document.querySelector(".showTrinketHangman")
 var showTrinketText = document.querySelector(".showTrinket span")
 trinket.style.display = "none"
-
-
-showTrinket.addEventListener("click", function() {
-    if (trinket.style.display === "none") {
-        $(".hiddenTrinketMain").removeClass("hideBackTrinket").addClass("hiddenTrinket")
-        trinket.style.display = "block"
-        showTrinketText.innerHTML = "<span>hide project</span>"
-    } else {
-        $(".hiddenTrinketMain").removeClass("hiddenTrinket").addClass("hideBackTrinket")
-        trinket.style.display = "none"
-        showTrinketText.innerHTML = "<span>view project</span>"
-    }
-})
