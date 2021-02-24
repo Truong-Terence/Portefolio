2d4e3fe380759905ce0dbd76a102eb4b30c30908
Térence renard--Truong <tvn.terence@gmail.com>
2021-02-24 20:42:36 +0100

    Python media

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,6 @@
  * Show and hide python project
  * Animation with keyframes
  */
-var trinket = document.querySelector(".hiddenTrinket")
-var showTrinketCasino = document.querySelector(".showTrinketCasino")
-var showTrinketDico = document.querySelector(".showTrinketDico")
+var trinketHangman = document.querySelector(".hiddenTrinketHangman")
 var showTrinketHangman = document.querySelector(".showTrinketHangman")
-var showTrinketText = document.querySelector(".showTrinket span")
-trinket.style.display = "none"
+trinketHangman.style.display = "none"
--- assets/js/secondPage.js
+++ assets/js/secondPage.js
@@ -1,7 +1,37 @@
+var trinketCasino = document.querySelector(".hiddenTrinketCasino")
+var showTrinketCasino = document.querySelector(".showTrinketCasino")
+trinketCasino.style.display = "none"
+
+var trinketDico = document.querySelector(".hiddenTrinketDico")
+var showTrinketDico = document.querySelector(".showTrinketDico")
+trinketDico.style.display = "none"
+
 showTrinketHangman.addEventListener("click", function() {
-    if (trinket.style.display === "none") {
-        trinket.style.display = "block"
+    if (trinketHangman.style.display === "none") {
+        trinketHangman.style.display = "block"
+        trinketCasino.style.display = "none"
+        trinketDico.style.display = "none"
     } else {
-        trinket.style.display = "none"
+        trinketHangman.style.display = "none"
+    }
+})
+
+showTrinketDico.addEventListener("click", function() {
+    if (trinketDico.style.display === "none") {
+        trinketDico.style.display = "block"
+        trinketCasino.style.display = "none"
+        trinketHangman.style.display = "none"
+    } else {
+        trinketDico.style.display = "none"
+    }
+})
+
+showTrinketCasino.addEventListener("click", function() {
+    if (trinketCasino.style.display === "none") {
+        trinketCasino.style.display = "block"
+        trinketDico.style.display = "none"
+        trinketHangman.style.display = "none"
+    } else {
+        trinketCasino.style.display = "none"
     }
 })
