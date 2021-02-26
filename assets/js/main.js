88562f992f8b079aeace22ee5b100a90af084b78
Térence renard--Truong <tvn.terence@gmail.com>
2021-02-26 17:12:08 +0100

    Dark-mode no svg

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,11 @@
 var trinketHangman = document.querySelector(".hiddenTrinketHangman")
 var showTrinketHangman = document.querySelector(".showTrinketHangman")
 trinketHangman.style.display = "none"
+
+function darkMode() {
+    document.body.classList.toggle("darkMode")
+    $("#skills").toggleClass("bg-white bg-box-darkMode")
+    $(".latestProjects").toggleClass("bg-white bg-box-darkMode")
+    $(".card").toggleClass("bg-box-darkMode")
+    $(".boxCard").toggleClass("boxCardDark")
+ }
